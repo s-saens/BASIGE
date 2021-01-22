0488a50ffefa42526a49f3cc172f310477097c12
Song_Saens <ssh9199_@naver.com>
2021-01-23 03:41:43 +0900

    project setting
    <Asset 추가>
    - DO Tween Plugin
    - Joystick Pack
    
    <샘플 코드 추가>
    - Controller (Joystick 에셋이용)
    
    <Package 적용>
    - URP, Shader Graph
    - Post Processing

---
+++ Assets/01_Dev/SimpleDebugger/SimpleDebugger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDebugger : MonoBehaviour
+{
+    
+    public GameObject[] objects;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
