2d0c50b14199274ac4752c2205513d65011b506e
HyeSuni <zaqxswcde88@naver.com>
2021-02-23 23:44:16 +0900

    죄송합니다 ㅠㅠ 이게 진짜에용

--- Assets/04_TestScenes/20_HS/Test2.cs
+++ Assets/04_TestScenes/20_HS/Test2.cs
@@ -10,7 +10,7 @@ public class Test2 : MonoBehaviour {
 
   void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
-        Animation.Play("Scratch");
+        GetComponent<Animation>().Play("Scratch");
          }
   }
 }
