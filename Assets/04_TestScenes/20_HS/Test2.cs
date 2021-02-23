111e21f69ac8798218425f8df87da0f1e17a3d87
HyeSuni <zaqxswcde88@naver.com>
2021-02-23 23:35:57 +0900

    스페이스 - 스킬 오류 수정

--- Assets/04_TestScenes/20_HS/Test2.cs
+++ Assets/04_TestScenes/20_HS/Test2.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class Test2 : MonoBehaviour {
   
   private Animation anim;
-
   void Starts() {
      anim = GetComponent<Animation>();
   }
 
   void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
-        anim.Play("Scratch");
+        GetComponent<Animation>().Play("Scratch");
          }
   }
 }
