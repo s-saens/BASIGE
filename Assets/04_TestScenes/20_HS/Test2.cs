32cdc477501ef9a5a8f529187a3d16fecdaaf31d
Song_Saens <ssh9199_@naver.com>
2021-03-01 17:27:13 +0900

    (HS) 애니메이션 앞발 transform 로컬화, CatPrefab으로

--- Assets/04_TestScenes/20_HS/Test2.cs
+++ Assets/04_TestScenes/20_HS/Test2.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class Test2 : MonoBehaviour {
-  
+public class Test2 : MonoBehaviour
+{
+
   private Animation anim;
-  void Starts() {
-     anim = GetComponent<Animation>();
+
+  void Starts()
+  {
+    anim = GetComponent<Animation>();
   }
 
-  void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-        GetComponent<Animation>().Play("Scratch");
-         }
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      GetComponent<Animation>().Play("Scratch");
+    }
   }
 }
